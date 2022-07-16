a7bd6de9bc4ef41dc6ffbe2ec5016d3d771349aa
banditben.1994@gmail.com <bobbest@DESKTOP-3UCTL45.localdomain>
2022-07-16 12:04:18 +0800

    feat[MVC] - Add POST/DELETE route

--- w10 - MVC/server.js
+++ w10 - MVC/server.js
@@ -15,14 +15,20 @@ const port = 3000;
 
 //Body parser - To pass the form values in to req.body as form data is form of url encoded
 //Need to tell express to use the request parsing middleware
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 //or ap.use(express.json())
-
 
 //Specifying the Template Engine to use - to render html page
 //Tell express which template engine to use
 //To do that need to type - npm install ejs
 app.set("view engine", "ejs");
+
+//include the method-override package
+const methodOverride = require("method-override");
+//...
+//after app has been defined
+//use methodOverride.  We'll be adding a query parameter to our edit form named _method
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -37,6 +43,10 @@ app.get("/pokemon/new", pokemonController.newPokemonForm);
 
 //Setting up show route for individual pokemon
 app.get("/pokemon/:pokemon_id", pokemonController.showPokemon);
+//update action - POST request
+app.put("/pokemon/:pokemon_id", pokemonController.updatePokemon);
+//delete action - delete request
+app.delete("/pokemon/:pokemon_id", pokemonController.deletePokemon);
 
 //create action
 app.post("/pokemon", pokemonController.createPokemon);
@@ -44,3 +54,6 @@ app.post("/pokemon", pokemonController.createPokemon);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+//How to Refactor This?
+//Create new folder -> Routes
